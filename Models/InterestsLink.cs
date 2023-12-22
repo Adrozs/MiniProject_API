061fe1d5f3bbf82ddbe9b8c2d78332a724a947a9
Adrozs <adrozsahegyi@gmail.com>
2023-12-22 22:30:52 +0100

    Added json property names so api can find the correct properites

--- Models/InterestsLink.cs
+++ Models/InterestsLink.cs
@@ -1,8 +1,20 @@
-﻿namespace API_Project.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Project.Models
 {
+    [Table("InterestLink")]
     public class InterestsLink
     {
         public int Id { get; set; }
         public string? WebLink { get; set; }
+
+        [ForeignKey("Person")]
+        public string PersonId { get; set; }
+        public Person Person { get; set; }
+
+        [ForeignKey("Interest")]
+        public string InterestId { get; set; }
+        public Interest Interest { get; set; }
     }
+
 }
