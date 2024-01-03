5618f9bfcc8e0f1f306ff446fd5b96a6bb2e0571
Adrozs <adrozsahegyi@gmail.com>
2024-01-03 23:14:07 +0100

    Added hierarchical endpoint and cleaned up text

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace API_Project
     {
         public static void Main(string[] args)
         {
+            // Setup
             var builder = WebApplication.CreateBuilder(args);
             string connectionString = builder.Configuration.GetConnectionString("ApplicationContext");
             builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
@@ -17,40 +18,58 @@ namespace API_Project
             app.MapGet("/", () => "Welcome to CATPAPI, the ChasAcademy Teaching Platform API!\n\n" +
             "How to use CATPAPI:\n" +
             "-{} brackets are where you fill out data\n" +
-            "-{search?} means it's optional\n" +
+            "-{?} means it's optional\n" +
             "-Search works by typing ex: \"/people/Ad\" and you'll get all students whose names start with \"Ad\" .\n" +
             "-Student IDs are the first 3 letters of their first- and last names (Bilbo Baggins = BILBAG)\n" +
             "-Interest IDs are the first 3 letters (Golf = GOL)\n\n" +
-            "Here are the available GET commands:\n" +  
+
+
+            "Here are the available GET commands:\n\n" +
+
+            "PEOPLE:\n" +
+
             "/people - Displays all student names and their IDs\n" +
-            "/people/{search} - Displays all students whose name starts with the search\n" +
+            "/people/{search?} - Displays all students whose name starts with the search\n" +
+            "/people/{personId}/hierarchical - get out all interests and all links for that student hierarchically\n" +
+            "/people/page/{page?}/results/{results?}/{search?} - Get all students and choose page and how many students shown per page. Option to filter students on pagination available with search\n\n" +
+
+            "INTERESTS:\n" +
             "/interests - Displays all interests and their IDs\n" +
-            "/interests/{search} - Displays all interests whose title starts with the search\n" +
+            "/interests/{search?} - Displays all interests whose title starts with the search\n\n" +
+
+            "PEOPLE & INTERESTS:\n" +
             "/people/{personId}/interests - Displays all interests for the chosen student\n" +
-            "/people/{personId}/interests/links - Display all interest links connected to a student\n\n" +
+            "/people/{personId}/interests/links - Display all interest links connected to a student\n\n\n" +
+
+
             "Here are the available POST commands:\n" +
             "/people/ - Add a new student\n" +
             "/interests - Add a new interest\n" +
             "/people/{personId}/interests/{interestId} - Connect a student to an existing interest\n" +
             "/people/{personId}/interests/{interestId}/links/ - Add a link to a student and their chosen interest\n");
 
-            // People commands
+
+            // ENDPOINTS
+
+            // People endpoints
             app.MapGet("/people/{search?}", PeopleHandler.GetPeople);
             app.MapGet("/people/page/{page?}/results/{results?}/{search?}", PeopleHandler.GetPeople);
+            app.MapGet("/people/{personId}/hierarchical", PeopleHandler.GetPersonHierarchical);
             app.MapPost("/people", PeopleHandler.AddPerson);
 
-            // Interests commands
+            // Interests endpoints
             app.MapGet("/interests/{search?}", InterestsHandler.GetInterests);
             app.MapPost("/interests", InterestsHandler.AddInterest);
 
-            // People interest commands
+            // People interest endpoints
             app.MapGet("/people/{personId}/interests/{search?}", InterestsHandler.GetPersonInterests);
             app.MapPost("/people/{personId}/interests/{interestId}", PeopleHandler.ConnectPersonToInterest);
 
-            // Interests link commands
+            // Interests link endpoints
             app.MapGet("/people/{personId}/interests/links", PeopleHandler.GetPersonLinks);
             app.MapPost("/people/{personId}/interests/{interestId}/links/", InterestsHandler.AddInterestLink);
             
+
             app.Run();
 
 
@@ -66,7 +85,7 @@ namespace API_Project
             // [x] Get all links that are connected to a specific person
 
             // EXTRA CHALLENGES
-            // [] Give the option for the one calling the API and asking for a person to get out all interests
+            // [x] Give the option for the one calling the API and asking for a person to get out all interests
             // and all links for that person directly in a hierarchical JSON-file
 
             // [x] Give the option for the one calling the API to filter what they get back, like a search.
