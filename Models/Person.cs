fd93f89b9f1a2efefe526cd415e058f449137c20
Adrozs <adrozsahegyi@gmail.com>
2024-01-03 23:14:27 +0100

    Made phone number optional (nullable)

--- Models/Person.cs
+++ Models/Person.cs
@@ -9,7 +9,7 @@ namespace API_Project.Models
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int PhoneNumber { get; set; }
+        public int? PhoneNumber { get; set; }
 
         public virtual ICollection<Interest> Interests { get; set; }
         public virtual ICollection<InterestsLink> InterestsLinks { get; set; }
